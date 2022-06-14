2642a4bca4d9ffa4951575a6399f648837e7a6c7
aqib-mehmood <aqimehmoodb@gmail.com>
2022-06-14 15:41:34 +0500

    Deployed token MHT to Goerlu Network

---
+++ hardhat.config.js
@@ -0,0 +1,38 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "Rman81QWYEFCI2qyYs1-7YmIpvVI5UXV";
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const GOERLI_PRIVATE_KEY = "739de05779044cded90a80021ca09460f31fcdabc435cf9c28dc69734e8d6d33";
+
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${GOERLI_PRIVATE_KEY}`]
+    }
+  }
+};
